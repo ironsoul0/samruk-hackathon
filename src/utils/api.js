1d765f4e81794b1fd4262f2ca9647f37d8c6b9f2
ironsoul0 <timka2609@gmail.com>
2021-03-15 01:43:00 +0600

    fix: add correct api route

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const base =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8080"
-    : "https://samruk-backend.ironsoul.me";
+    : "https://morning-peak-19354.herokuapp.com";
 
 export const api = axios.create({
   baseURL: `${base}/`,
