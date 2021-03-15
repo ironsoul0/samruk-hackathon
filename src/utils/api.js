331e61e44dca2434f5dcc1480b97e5c713b06da4
ironsoul0 <timka2609@gmail.com>
2021-03-15 12:44:42 +0600

    fix: put correct api route

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const base =
-  process.env.NODE_ENV !== "development"
+  process.env.NODE_ENV === "development"
     ? "http://localhost:8080"
     : "https://morning-peak-19354.herokuapp.com";
 
